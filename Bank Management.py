77e811bdb16b90102d9cd0df48a738e7f8b45704
Neeha-Sultana <104249476+Neeha-Sultana@users.noreply.github.com>
2024-05-15 23:15:20 +0530

    Update Bank Management.py

--- Bank Management.py
+++ Bank Management.py
@@ -14,7 +14,6 @@ while True:
     a.close()
 #1. SIGN UP
 def signup():
-
     while(True):
         print("~~~~~~~~~~~~~~~~~~~SIGN UP~~~~~~~~~~~~~~~~~~~~~~~~~")
         a = input("USERNAME(more than 3 characters): ")
